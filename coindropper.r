1be9f8567dd23e49eec23501f68d4f6a87d9ef4b
Matt Malin <matthewmalin@gmail.com>
2018-08-30 16:20:02 +0100

    Re-introducing symmetric idea to add 'mirrored' observations
    The observations are now duplicated and mirrored, i.e. if the first starting bucket is observed to land in the second end bucket, it also counts as an observation of the last starting bucket and the second from last end bucket.

--- coindropper.r
+++ coindropper.r
@@ -4,19 +4,26 @@ library(tidyverse)
 # | | | | | | | | |
 # so each start_bucket is 9/8 the size of an end bucket
 
-# recorded data has singl eline with start_bucket and end_bucket observations:
+# recorded data has single line with start_bucket and end_bucket observations:
 experience_individuals <- read_csv("experience.txt", col_names = c("start_bucket", "end_bucket"))
+
+experience_individuals <- bind_rows(
+  experience_individuals, 
+  mutate(experience_individuals,
+    start_bucket = 9 - start_bucket,
+    end_bucket   =  10 - end_bucket))
 
 experience <- experience_individuals %>% 
   group_by(start_bucket, end_bucket) %>% 
   summarise(count = length(end_bucket))
 
+
 ggplot(experience, aes(x = end_bucket, y = count)) + 
   geom_bar(stat = "identity", aes(fill = factor(end_bucket))) +
-  facet_wrap(~ start_bucket) +
   labs(
     title = "End bucket distribution by starting bucket",
     fill = "End bucket",
     x = "End Bucket"
   ) +
-  scale_x_continuous(breaks = 1:9)
+  scale_x_continuous(breaks = 1:9) +
+  facet_wrap(~ start_bucket, scales = "free_y")

9729d5a4d325e9aab1cf1456e6ea2b6bedb2bb27
Matt Malin <matthewmalin@gmail.com>
2018-08-30 21:02:22 +0100

    adding distribution fitting and estimates, including visualisations

--- coindropper.r
+++ coindropper.r
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(fitdistrplus)
+
 # there are 8 start buckets, and 9 end buckets
 # | | | | | | | | |
 # so each start_bucket is 9/8 the size of an end bucket
@@ -8,26 +10,50 @@ library(tidyverse)
 # recorded data has single line for each start_bucket and end_bucket observations:
 experience_individuals <- read_csv("experience.txt", col_names = c("start_bucket", "end_bucket"))
 
-# add 'mirrored' observations under assumption of symmetry:
-experience_individuals <- bind_rows(
-  experience_individuals, 
-  mutate(experience_individuals,
-    start_bucket = 9 - start_bucket,
-    end_bucket   =  10 - end_bucket))
-
+# for ease of later distribution assumptions, label end buckets 0 to 8 (rather than 1 to 9),
+# and add 'mirrored' observations under assumption of symmetry:
 experience <- experience_individuals %>% 
+  mutate(
+    start_bucket = 9 - start_bucket,
+    end_bucket   =  10 - end_bucket) %>%
+  bind_rows(experience_individuals) %>% 
+  mutate(end_bucket = end_bucket - 1) %>% 
   group_by(start_bucket, end_bucket) %>% 
-  summarise(count = length(end_bucket))
-
-experience_rescaled <- experience %>% 
+  summarise(count = length(end_bucket)) %>%
   group_by(start_bucket) %>% 
   mutate(proportion = count / sum(count)) %>% 
   ungroup()
 
+# Distribution fitting ----------------------------------------------------
+
+# note that binomial distribution has chance of 0, i.e. with size 10 there can be between 0 and 10
+# successes, but here there is always a bucket, i.e. 1-9 rather than 0-9, so offset by 1
+
+distribution_fitting <- experience %>% 
+  complete(start_bucket, end_bucket, fill = list(count = 0, proportion = 0)) %>% 
+  group_by(start_bucket) %>%
+  nest() %>% 
+  group_by(start_bucket) %>%
+  mutate(
+    fitted_distribution = map(data, function(x) {
+      fitdist(rep(x$end_bucket, x$count), "binom", fix.arg = list(size = 8), start = list(prob = start_bucket / 9))
+    })
+  ) %>% 
+  mutate(
+    estimate = map_dbl(fitted_distribution, function(x) x[["estimate"]][["prob"]])
+  )
+
+experience_fitted <- distribution_fitting %>%
+  unnest(.preserve = fitted_distribution) %>% 
+  mutate(
+    estimate_proportion = dbinom(end_bucket, 8, estimate)
+  ) %>% 
+  group_by(start_bucket)
+
 
 # Visualisations ----------------------------------------------------------
 
-ggplot(experience, aes(x = end_bucket, y = count)) + 
+ggplot(experience, aes(x = end_bucket, y = proportion)) + 
   geom_bar(stat = "identity", aes(fill = factor(end_bucket))) +
   labs(
     title = "End bucket distribution by starting bucket",
@@ -35,7 +61,7 @@ ggplot(experience, aes(x = end_bucket, y = count)) +
     x = "End Bucket"
   ) +
   scale_x_continuous(breaks = 1:9) +
-  facet_wrap(~ start_bucket, scales = "free_y")
+  facet_wrap(~ start_bucket)
 
 
 ggplot(experience_rescaled, aes(x = start_bucket, y = proportion)) + 
@@ -49,58 +75,21 @@ ggplot(experience_rescaled, aes(x = start_bucket, y = proportion)) +
   facet_wrap(~ end_bucket, scales = "free_y")
 
 
-
-# Distribution fitting ----------------------------------------------------
-
-experience %>% 
-  group_by(start_bucket) %>% 
-  summarise(sum = sum(count)) %>% 
-  arrange(desc(sum))
-
-experience %>% 
-  filter(start_bucket == 4)
-
-library(fitdistrplus)
-
-sample_points <- rbinom(100, 10, 0.5)
-
-fitdist(
-  data = sample_points,
-  distr = "binom",
-  fix.arg = list(size = 10),
-  start = list(prob = 0.4)
-  )
+ggplot(distribution_fitting, aes(x = start_bucket, y = estimate)) +
+  geom_point(size = 5)
 
 
+ggplot(experience_fitted, aes(x = end_bucket, y = proportion)) + 
+  geom_bar(stat = "identity", aes(fill = factor(end_bucket))) +
+  geom_point(aes(y = estimate_proportion)) +
+  geom_line(aes(y = estimate_proportion)) +
+  labs(
+    title = "End bucket distribution by starting bucket",
+    fill = "End bucket",
+    x = "End Bucket"
+  ) +
+  scale_x_continuous(breaks = 1:9) +
+  facet_wrap(~ start_bucket, scales = "free_y")
 
-fitdist(
-  data = sample_points,
-  distr = "binom",
-  fix.arg = list(size = 10),
-  start = list(prob = 0.4)
-)
-
-
-experience %>% 
-  ungroup() %>% 
-  filter(start_bucket == 4) %>% 
-  (function(x) {rep(x$end_bucket, x$count)}) %>% 
-  fitdist("binom", fix.arg = list(size = 9), start = list(prob = 0.4))
-
-example_bucket_4 <- experience %>% 
-  ungroup() %>% 
-  filter(start_bucket == 4) %>% 
-  (function(x) {rep(x$end_bucket, x$count)}) %>% 
-  fitdist("binom", fix.arg = list(size = 9), start = list(prob = 0.4))
-
-str(example_bucket_4)
-
-# note that binomial distribution has chance of 0, i.e. with size 10 there can be between 0 and 10
-# successes, but here there is always a bucket, i.e. 1-9 rather than 0-9
-# so may need to use size 8 rather than 9 - TBD
-
-
-
-library(broom)
 
 
